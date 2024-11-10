58fb3e441da11d98699e2d30612be6146b301a2b
kailas <kailasnath727@gmail.com>
2024-11-10 14:49:38 +0530

    changed landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
         <main className="flex-grow"> {/* This allows main to take up available space */}
           <Routes>
-            <Route path="/" element={<LandingPage />} /> {/* Landing Page */}
+            <Route path="/" element={<SurveyPage />} /> {/* Landing Page */}
             <Route path="/survey" element={<SurveyPage />} /> {/* Survey Page */}
           </Routes>
         </main>
